1587972c71899cf1f500ba1ab9745ae63106d13a
Talha <muhammadtalha.sultan@georgebrown.ca>
2019-12-27 17:50:29 -0500

    added picture and buildings btn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  building = '';
+  constructor(private pickerCtrl: PickerController) {}
 
+  async buildingsPicker(){
+    let pickerAction;
+    let buildings: PickerOptions = {
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done',
+          role: 'done',
+          handler: value => {
+            pickerAction = 'done';
+          }
+        }
+      ],
+      columns: [
+        {
+          name: 'building',
+          options: [
+            { text: '--- Please Select ---', value: 'null'},
+            { text: 'Building C', value: 'buildingC' },
+            { text: 'Building D', value: 'buildingD' },
+            { text: 'Building E', value: 'buildingE' }
+          ]
+        }
+      ]
+    };
+
+    let picker = await this.pickerCtrl.create(buildings);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('building');
+      if (pickerAction == 'done') {
+        console.log('col: ', col.options[col.selectedIndex].value);
+        this.building = col.options[col.selectedIndex].text;
+      }
+    });
+  }
 }
