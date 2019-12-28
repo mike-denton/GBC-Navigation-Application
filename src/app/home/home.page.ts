4f729ab07020f8120b4abd95b5a19ee780a5199f
Talha <muhammadtalha.sultan@georgebrown.ca>
2019-12-28 18:02:29 -0500

    canvas working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { PickerController } from '@ionic/angular';
 import { PickerOptions } from '@ionic/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  building = '';
-  constructor(private pickerCtrl: PickerController) {}
+export class HomePage {
+  
+  @ViewChild('canvasEl', {static: true}) canvasEl: ElementRef;  
+  private context: CanvasRenderingContext2D;
+  private width: 578;
+  private height: 438;
+
+  private destinationMarker = new Image();
+
+  private originMarker = new Image();
+
+  private parkingA;
+
+  constructor(private pickerCtrl: PickerController,private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+
+    this.destinationMarker.src = 'assets/images/map-destination-marker.png';
+
+    this.originMarker.src = 'assets/images/map-origin-marker.png';
+
+    this.parkingA = document.getElementById("parkingA");
+    this.parkingA.addEventListener("click", (e:Event) => {
+      this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+      this.drawOriginMarker() 
+    });
+  }
+
+  private drawOriginMarker(){
+    this.context.drawImage(this.originMarker, 315,260);
+  }
+
+  private drawPath(btnId : string) {
+    this.context.beginPath();
+    this.context.lineWidth = 5;
+    this.context.strokeStyle = "lime";
+    switch (btnId){
+      case 'buildingC':
+        this.context.moveTo(324,270);
+        this.context.lineTo(280, 270);
+        this.context.lineTo(280, 139);
+        this.context.lineTo(230, 139);
+        this.drawDestinationMarker(btnId);
+        break;
+      case 'buildingD':
+        this.context.moveTo(324,270);
+        this.context.lineTo(280, 270);
+        this.context.lineTo(280, 230);
+        this.context.lineTo(225, 229);
+        this.context.lineTo(205, 240);
+        this.context.lineTo(153, 240);
+        this.context.lineTo(153, 210);
+        this.context.lineTo(190, 210);
+        this.drawDestinationMarker(btnId);
+        break;
+      case 'buildingE':
+          this.context.moveTo(324,270);
+          this.context.lineTo(280, 270);
+          this.context.lineTo(280, 230);
+          this.context.lineTo(243, 230);
+          this.context.lineTo(243, 197);
+          this.drawDestinationMarker(btnId);
+          break;
+      default:
+        break;
+    }
+    this.context.stroke();
+  }
+
+  private drawDestinationMarker(btnId : String){
+    switch (btnId){
+      case 'buildingC':
+        this.context.drawImage(this.destinationMarker, 215,110);
+        break;
+      case 'buildingD':
+        this.context.drawImage(this.destinationMarker, 175,180);
+        break;
+      case 'buildingE':
+        this.context.drawImage(this.destinationMarker, 227, 165);
+        break;
+      default:
+        break;
+    }
+  }
 
   async buildingsPicker(){
     let pickerAction;
@@ -30,7 +113,7 @@ export class HomePage {
       ],
       columns: [
         {
-          name: 'building',
+          name: 'buildings',
           options: [
             { text: '--- Please Select ---', value: 'null'},
             { text: 'Building C', value: 'buildingC' },
@@ -44,10 +127,14 @@ export class HomePage {
     let picker = await this.pickerCtrl.create(buildings);
     picker.present();
     picker.onDidDismiss().then(async data => {
-      let col = await picker.getColumn('building');
+      let col = await picker.getColumn('buildings');
       if (pickerAction == 'done') {
-        console.log('col: ', col.options[col.selectedIndex].value);
-        this.building = col.options[col.selectedIndex].text;
+        if (col.options[col.selectedIndex].value == 'null') {
+          this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        }
+        this.drawPath(col.options[col.selectedIndex].value);
+      }else{
+        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
       }
     });
   }
